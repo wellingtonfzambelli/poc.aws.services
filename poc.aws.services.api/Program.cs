695d65098706725524b3c0aed081c72b9d47a50d
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-24 20:38:03 -0300

    .

--- poc.aws.services.api/Program.cs
+++ poc.aws.services.api/Program.cs
@@ -1,17 +1,11 @@
-using System.Reflection;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var version = Environment.GetEnvironmentVariable("VERSION") ?? "1.0.0";
-
-
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
-        Title = "POC AWS Services Api",
-        Version = version
+        Title = "POC AWS Services Api"
     });
 });
 
@@ -27,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", $"POC AWS Services Api {version}");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", $"POC AWS Services Api");
     });
 }
 
