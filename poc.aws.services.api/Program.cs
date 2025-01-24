dd8a1becf7ad625cff625bc558faff703b50993a
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-24 19:24:03 -0300

    yml adjust

--- poc.aws.services.api/Program.cs
+++ poc.aws.services.api/Program.cs
@@ -8,13 +8,12 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
-        Title = "My API",
+        Title = "POC AWS Services Api",
         Version = version
     });
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", $"My API {version}");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", $"POC AWS Services Api {version}");
     });
 }
 
