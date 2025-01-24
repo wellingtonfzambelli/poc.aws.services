81cc4fbfc1f2d6b71a0d9de67b8368ae53839a3d
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-24 16:29:49 -0300

    add application version

--- poc.aws.services.api/Program.cs
+++ poc.aws.services.api/Program.cs
@@ -1,6 +1,17 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "1.0.0";
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "My API",
+        Version = version
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +24,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", $"My API {version}");
+    });
 }
 
 app.UseHttpsRedirection();
